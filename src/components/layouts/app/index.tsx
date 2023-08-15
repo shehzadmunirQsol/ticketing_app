78ad31dfac653ec39ea19b67b5f32f868e0e3c28
Ameen Ilyas <ameenilyas786q.sols@gmail.com>
2023-08-15 19:05:01 +0500

    implemented language convertor

--- src/components/layouts/app/index.tsx
+++ src/components/layouts/app/index.tsx
@@ -1,17 +1,23 @@
 import React, { ReactNode } from 'react';
 import Header from './header';
+import { useSelector } from 'react-redux';
+import { RootState } from '~/store/store';
 
 type DefaultLayoutProps = { children: ReactNode };
 
-function index({ children }: DefaultLayoutProps) {
+function Index({ children }: DefaultLayoutProps) {
+  const { lang } = useSelector((state: RootState) => state.layout);
+
   return (
-    <>
-      <div className="relative w-full overflow-x-hidden">
-        <Header />
-        <div className="w-full">{children}</div>
-      </div>
-    </>
+    <div
+      dir={lang.dir}
+      lang={lang.lang}
+      className="relative w-full overflow-x-hidden"
+    >
+      <Header />
+      <div className="w-full">{children}</div>
+    </div>
   );
 }
 
-export default index;
+export default Index;
--- src/store/reducers/layout.ts
+++ src/store/reducers/layout.ts
@@ -2,14 +2,20 @@ import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '~/store/store';
 
+interface Lang {
+  dir: 'rtl' | 'ltr';
+  lang: 'en' | 'ar';
+}
 // Define a type for the slice state
 interface LayoutState {
   theme: string;
+  lang: Lang;
 }
 
 // Define the initial state using that type
 const initialState: LayoutState = {
   theme: 'dark',
+  lang: { dir: 'ltr', lang: 'en' },
 };
 
 export const layoutSlice = createSlice({
@@ -20,10 +26,14 @@ export const layoutSlice = createSlice({
     setTheme: (state, action: PayloadAction<string>) => {
       state.theme = action.payload;
     },
+    toggleLang: (state, action: PayloadAction<Lang>) => {
+      console.log('action.payload', action.payload);
+      state.lang = action.payload;
+    },
   },
 });
 
-export const { setTheme } = layoutSlice.actions;
+export const { setTheme, toggleLang } = layoutSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectCount = (state: RootState) => state.layout;
