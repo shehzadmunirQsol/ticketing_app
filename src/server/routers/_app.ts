d93efbe140b94584abe3f760163a45d0106e02a3
Ameen Ilyas <ameenilyas786q.sols@gmail.com>
2023-08-17 18:00:06 +0500

    created base procedures for category and events

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,13 +1,13 @@
 /**
  * This file contains the root router of your tRPC-backend
  */
-import { publicProcedure, router } from '../trpc';
-// import { postRouter } from './post';
+import { router } from '../trpc';
+import { categoryRouter } from './category';
+import { eventRouter } from './event';
 
 export const appRouter = router({
-  healthcheck: publicProcedure.query(() => 'yay!'),
-
-  // post: postRouter,
+  category: categoryRouter,
+  event: eventRouter,
 });
 
 export type AppRouter = typeof appRouter;
