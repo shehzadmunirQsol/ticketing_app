9d103d80def91fe98b0d07e0e4ed94249b9bbe66
HassanShanJava <hassanshan675@gmail.com>
2023-09-14 15:47:52 +0500

    added ui fixes on product desc

--- src/components/app/productDescription/Tabs.tsx
+++ src/components/app/productDescription/Tabs.tsx
@@ -12,19 +12,19 @@ const Tabs = () => {
       <div className="px-10 py-8 w-full">
         <div className="flex flex-row -m-4 text-center justify-center items-start lg:items-center  gap-6 lg:gap-14 md:gap-14   lg:flex-row ">
           <div className="flex justify-center items-lg:flex-row center ">
-            <button className='bg-primary text-black text-xs lg:text-base font-extrabold px-4 py-2 rounded-full tracking-tighter'>
+            <button className='bg-background text-cardGray hover:bg-primary hover:text-black text-xs lg:text-base font-extrabold px-4 py-2 rounded-full tracking-tighter'>
               <Link href="#BuyTickets" className='font-sans'>BUY TICKETS</Link>
 
             </button>
           </div>
           <div className="flex justify-center items-center ">
-            <button className='bg-background text-cardGray text-xs lg:text-base font-extrabold px-6 py-2 rounded-full tracking-tighter'>
+            <button className='bg-background text-cardGray hover:bg-primary hover:text-black text-xs lg:text-base font-extrabold px-6 py-2 rounded-full tracking-tighter'>
 
               <Link href="#CompititionDetail" className='font-sans'>COMPETITIONS DETAILS</Link>
             </button>
           </div>
           <div className="flex justify-center items-center ">
-            <button className='bg-background text-cardGray text-xs lg:text-base font-extrabold px-6 py-2 rounded-full tracking-tighter'>
+            <button className='bg-background text-cardGray hover:bg-primary hover:text-black text-xs lg:text-base font-extrabold px-6 py-2 rounded-full tracking-tighter'>
               <Link href="#AccordianFaqs" className='font-sans'>FAQs</Link>
             </button>
           </div>
