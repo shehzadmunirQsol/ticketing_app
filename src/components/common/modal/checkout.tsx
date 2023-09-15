9337bbcfabcd34703f80595beab4e57b95c4affc
unknown <shehzadmunir.qsols@gmail.com>
2023-09-15 19:43:44 +0500

    work on environment

--- src/components/common/modal/checkout.tsx
+++ src/components/common/modal/checkout.tsx
@@ -153,7 +153,7 @@ export function CheckoutDialog(props: SettingDialogInterface) {
             }}
           ></Script>
           <form
-            action={`${process.env.NEXT_PUBLIC_BASE_URL}checkout`}
+            action={`${process.env.NEXT_PUBLIC_BASE_URL}/checkout`}
             className="paymentWidgets justify-start   lg:justify-center md:justify-center items-center px-2 lg:px-6 py-2 space-y-2 text-black"
             data-brands="VISA MASTER AMEX "
           ></form>
