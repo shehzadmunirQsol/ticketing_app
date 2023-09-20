c8cb972644396b2c005f625331c3c5b8161c4e75
HassanShanJava <hassanshan675@gmail.com>
2023-09-20 21:44:26 +0500

    Added ui fixes

--- src/components/app/home/banner_slider.tsx
+++ src/components/app/home/banner_slider.tsx
@@ -129,7 +129,7 @@ const BannerSlider = () => {
               showElement ? 'fading-animation' : ''
             } transition-all  duration-500 ease-in-out items-center   top-32 sm:top-48 h-fit mx-auto  ltr:md:ml-20 rtl:md:mr-20 text-white sm:max-w-[500px] lg:max-w-[700px]`}
           >
-            <p className="px-4 text-2xl  sm:text-4xl md:text-5xl xl:text-[64px] font-[900] tracking-[0.2em] ">
+            <p className="px-4 text-2xl  sm:text-4xl md:text-5xl xl:text-[64px] font-[900] -tracking-[-2.56px] ">
               {carSlider[currentIndex]?.title}
             </p>
             {carSlider[currentIndex]?.price ? (
