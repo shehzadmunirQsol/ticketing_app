629dc3727a4b8119e711f24ea1e3aa50eeba87c6
unknown <shehzadmunir.qsols@gmail.com>
2023-09-20 19:03:59 +0500

    work on  forget password and product description

--- src/components/app/productDescription/VideoSection.tsx
+++ src/components/app/productDescription/VideoSection.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import ImageSlider from './ImageSlider';
-import EntiresDetail from './EntiresDetail';
-import VideoCard from '~/components/common/video_card';
 
-const VideoSection = () => {
+const VideoSection = (props: any) => {
   return (
     <div className="mt-10 mb-20">
-      <iframe
-        className="w-full h-[560px]"
-        src="https://www.youtube.com/embed/Y-x0efG1seA?controls=0"
-        title=""
-        // frameBorder="0"
-        allow=" autoplay;"
-        allowFullScreen
-      ></iframe>    
+      {props?.data?.video_src && (
+        <iframe
+          className="w-full h-[560px]"
+          src={props?.data?.video_src}
+          title=""
+          // frameBorder="0"
+          allow=" autoplay;"
+          allowFullScreen
+        ></iframe>
+      )}
     </div>
   );
 };
