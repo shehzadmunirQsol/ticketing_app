4cfdb6debf7e106b7d214aaaf1eb612bfb643295
unknown <shehzadmunir.qsols@gmail.com>
2023-09-22 16:15:43 +0500

    work on revalidation

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -40,7 +40,7 @@ export async function getStaticProps({ params }: any) {
     },
   });
 
-  return { props: { storeBlogsData: response } };
+  return { props: { storeBlogsData: response }, revalidate: 60 };
 }
 
 const CmsFunc = dynamic(() => import('~/components/app/cms/index'), {
