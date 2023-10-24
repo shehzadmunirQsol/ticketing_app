42807a26bffc05036ff36b7c43a7dbacef3bd0e1
Ameen Ilyas <ameenilyas786q.sols@gmail.com>
2023-10-24 11:39:21 +0500

    resolved naming issues in file importing

--- src/components/app/checkout/index.tsx
+++ src/components/app/checkout/index.tsx
@@ -37,7 +37,7 @@ import jqeury from 'jquery';
 import { CardDailog } from '~/components/common/modal/cardModal';
 import langContent from '~/locales';
 import visa from '~/public/assets/icons/visa.svg';
-import master from '~/public/assets/icons/master.svg';
+import master from '~/public/assets/icons/Master.svg';
 import Paypal from '~/public/assets/icons/Paypal.svg';
 import applePay from '~/public/assets/icons/applePay.svg';
 
