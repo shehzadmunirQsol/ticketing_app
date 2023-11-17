daf26e2775deb40e5319de17c6e2be3ddd5d0003
unknown <shehzadmunir.qsols@gmail.com>
2023-11-17 14:26:06 +0500

    work on customer detail section

--- src/components/admin/customers/detail.tsx
+++ src/components/admin/customers/detail.tsx
@@ -73,7 +73,7 @@ const CustomerDetailView = (props: any) => {
         {profileFields.map((field) => (
           <div
             key={field.key}
-            className="grid grid-cols-2 md:grid-cols-3 my-2 items-center gap-2"
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 my-2 items-center gap-2"
           >
             <div>{field.label}: </div>
             <div>{props?.customerDetail?.[field.key]}</div>
