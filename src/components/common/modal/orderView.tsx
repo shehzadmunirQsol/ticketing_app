4bcc0489e555524932bb6893e295fd44dcfd062e
ahmarsm <ahmar@lemonadverts.com>
2024-01-12 15:21:24 +0400

    invoice design

--- src/components/common/modal/orderView.tsx
+++ src/components/common/modal/orderView.tsx
@@ -101,7 +101,7 @@ export function OrderViewDialog(props: OrderViewDialogInterface) {
   return (
     <>
       <Dialog open={props?.isModal} onOpenChange={(e) => props.setIsModal(e)}>
-        <DialogContent className="h-[60vh] max-w-xl md:max-w-[768px] overflow-y-hidden scroll-hide p-2">
+        <DialogContent className="h-[60vh] max-w-xl md:max-w-[900px] overflow-y-hidden scroll-hide p-2">
           <ScrollArea className="h-100 p-4">
             <ScrollBar orientation="vertical"></ScrollBar>
             <DialogFooter className=" sm:justify-start items-start w-full mb-2">
@@ -116,30 +116,36 @@ export function OrderViewDialog(props: OrderViewDialogInterface) {
             </DialogFooter>
             <DialogDescription className="relative bg-card h-full rounded-lg  overflow-y-scroll scroll-hide">
               {OrderApiData && (
-                <div
-                  className="bg-card h-full text-white rounded-lg  px-8 py-6  mx-auto  "
-                  id="divToPrint"
-                >
-                  <div className="flex flex-col md:flex-row items-center justify-between mb-5">
-                    <div className="flex items-center">
+                <div className="bg-card whiteText h-full rounded-lg  px-4 md:px-8 py-4 md:py-6  mx-auto" id="divToPrint">
+                  <div className="flex flex-col md:flex-row items-start md:items-center justify-between mb-5">
+                    <div className="flex flex-col mb-5 md:mb-0">
                       <NextImage
                         className="h-16  object-contain mr-2"
                         src={LogoImage}
                         alt="Logo"
                       />
+                      <div className="text-sm">
+                        Winnar Trading LLC
+                      </div>
+                      <div className="text-sm greyText">
+                        Office 1716 - F17, The Bay Gate Tower, Al Mustaqbal Street, <br/> Business Bay, Dubai, 74490
+                      </div>
                     </div>
-                    <div className="text-white xs:text-center sm:text-left">
-                      <div className="font-bold text-xl mb-2">INVOICE</div>
+                    <div className="text-left">
+                      <div className="font-bold text-xl mb-1">TAX INVOICE</div>
                       <div className="text-sm greyText">
                         Date: {displayDate(OrderApiData?.data?.created_at)}
                       </div>
                       <div className="text-sm greyText">
                         Invoice: #INV00{OrderApiData?.data?.id}
                       </div>
+                      <div className="text-sm greyText">
+                        TRN: 104151951100003
+                      </div>
                     </div>
                   </div>
-                  <div className="border-b-2 border-gray-300 pb-5 mb-4">
-                    <h2 className="text-2xl  font-bold mb-4">Bill To:</h2>
+                  <div className="border-b-2 border-gray-300 text-sm pb-5">
+                    <h2 className="text-2xl font-bold mb-3">Bill To:</h2>
                     <p className="greyText">
                       {OrderApiData?.data?.first_name +
                         ' ' +
@@ -150,25 +156,25 @@ export function OrderViewDialog(props: OrderViewDialogInterface) {
                       {OrderApiData?.data?.city}, {OrderApiData?.data?.country}{' '}
                       {OrderApiData?.data?.postal_code}
                     </p>
-                    <p className="mt-2 greyText">{OrderApiData?.data?.email}</p>
+                    <p className="greyText">{OrderApiData?.data?.email}</p>
                   </div>
-                  <div className="w-full">
-                    <div className="w-full mb-8">
-                      <div className="flex justify-between font-bold uppercase py-2">
+
+                    <div className="w-full text-xs my-3">
+                      <div className="flex justify-between font-bold uppercase pb-2 smtext">
                         <div className="flex-[2] text-start">Name</div>
                         <div className="flex-1 text-center">Quantity</div>
-                        <div className="flex-1 text-center">Unit Price <span>(AED)</span></div>
-                        <div className="flex-1 text-center">Sub Total <span>(AED)</span></div>
-                        <div className="flex-1 text-center">VAT (5%)</div>
-                        <div className="flex-1 text-right">Total Amount</div>
+                        <div className="flex-1 text-center">Unit Price <span className="block text-xxs">(AED)</span></div>
+                        <div className="flex-1 text-center">Sub Total <span className="block text-xxs">(AED)</span></div>
+                        <div className="flex-1 text-center">VAT (5%) <span className="block text-xxs">(AED)</span></div>
+                        <div className="flex-1 text-right">Total Amount <span className="block text-xxs">(AED)</span></div>
                       </div>
 
                       {isFetching ? null : (
-                        <div className="mt-2">
+                        <div className="mt-0">
                           {OrderApiData?.data?.OrderEvent &&
                             OrderApiData?.data?.OrderEvent?.map(
                               (item: any, index: number) => (
-                                <div key={index} className="flex gap-2 py-2 greyText">
+                                <div key={index} className="flex gap-2 py-2 greyText smtext">
                                   <div className="flex-[2] text-start">
                                     {item?.Event?.EventDescription[0]?.name}
                                   </div>
@@ -179,18 +185,17 @@ export function OrderViewDialog(props: OrderViewDialogInterface) {
                                     {item?.quantity}
                                   </div>
                                   <div className="flex-1 text-center">
-                                    AED {reduceVATAmount(item?.ticket_price).toFixed(2)}
+                                    {reduceVATAmount(item?.ticket_price).toFixed(2)}
                                   </div>
                                   <div className="flex-1 text-center">
-                                    AED {reduceVATAmount(item?.ticket_price * item?.quantity).toFixed(2)}
+                                    {reduceVATAmount(item?.ticket_price * item?.quantity).toFixed(2)}
                                   </div>
 
                                   <div className="flex-1 text-center">
-                                    AED {getVATAmount(item?.ticket_price * item?.quantity).toFixed(2)}
+                                    {getVATAmount(item?.ticket_price * item?.quantity).toFixed(2)}
                                   </div>
 
                                   <div className="flex-1 text-right">
-                                    AED{' '}
                                     {(
                                       item?.ticket_price * item?.quantity
                                     ).toFixed(2)}
@@ -201,28 +206,25 @@ export function OrderViewDialog(props: OrderViewDialogInterface) {
                         </div>
                       )}
                     </div>
-                  </div>
 
-
-
-                  <div className="w-full border-t-2 border-gray-300 ">
-                    <div className="w-full mt-8">
-                      <div className="flex justify-between ">
-                        <div className="flex-[2] text-start font-bold uppercase py-2">Total Amount</div>
+                  <div className="w-full border-t-2 border-gray-300 text-xs smtext">
+                    <div className="w-full my-2">
+                      <div className="flex justify-between py-2">
+                        <div className="flex-[2] text-start font-bold uppercase">Total Amount</div>
                         <div className="flex-1"></div>
                         <div className="flex-1"></div>
                         <div className="flex-1 text-center greyText">{reduceVATAmount(OrderApiData?.data?.sub_total_amount).toFixed(2)}</div>
                         <div className="flex-1 text-center greyText">{getVATAmount(OrderApiData?.data?.sub_total_amount).toFixed(2)}</div>
-                        <div className="flex-1 text-center greyText">{(OrderApiData?.data?.total_amount).toFixed(2)}</div>
+                        <div className="flex-1 text-right greyText">{(OrderApiData?.data?.total_amount).toFixed(2)}</div>
                       </div>
 
-                      <div className="flex justify-between ">
-                        <div className="flex-[2] text-start font-bold uppercase py-2">Discount Coupon</div>
+                      <div className="flex justify-between py-2">
+                        <div className="flex-[2] text-start font-bold uppercase">Discount Coupon</div>
                         <div className="flex-1"></div>
                         <div className="flex-1"></div>
                         <div className="flex-1"></div>
                         <div className="flex-1"></div>
-                        <div className="flex-1 text-center greyText">
+                        <div className="flex-1 text-right greyText">
                           {OrderApiData?.data?.discount_amount > 0
                             ? (OrderApiData?.data?.discount_amount).toFixed(2)
                             : '0.00'}
@@ -231,24 +233,24 @@ export function OrderViewDialog(props: OrderViewDialogInterface) {
                     </div>
                   </div>
 
-                  <div className="w-full border-t-2 border-gray-300 ">
-                    <div className="w-full mt-8">
-                      <div className="flex justify-between ">
-                        <div className="flex-[2] text-start font-bold py-2">AED <span className='greyText'>{numberToWords(OrderApiData?.data?.total_amount)}</span></div>
+                  <div className="w-full border-t-2 border-gray-300 text-xs smtext">
+                    <div className="w-full my-3">
+                      <div className="flex justify-between font-bold uppercase py-2">
+                        <div className="flex-[2] text-start">AED <span className='greyText'>{numberToWords(OrderApiData?.data?.total_amount)}</span></div>
                         <div className="flex-1"></div>
                         <div className="flex-1"></div>
                         <div className="flex-1 text-center"></div>
-                        <div className="flex-1 text-center">VAT Amount <span>(AED)</span></div>
-                        <div className="flex-1 text-center">Total Payable <span>(AED)</span></div>
+                        <div className="flex-1 text-center">VAT Amount <span className="block text-xxs">(AED)</span></div>
+                        <div className="flex-1 text-right">Total Payable <span className="block text-xxs">(AED)</span></div>
                       </div>
 
-                      <div className="flex justify-between ">
+                      <div className="flex justify-between py-2">
                         <div className="flex-[2]"></div>
                         <div className="flex-1"></div>
                         <div className="flex-1"></div>
                         <div className="flex-1 text-center"></div>
                         <div className="flex-1 text-center greyText">{getVATAmount(OrderApiData?.data?.sub_total_amount).toFixed(2)}</div>
-                        <div className="flex-1 text-center greyText">{(OrderApiData?.data?.total_amount).toFixed(2)}</div>
+                        <div className="flex-1 text-right greyText">{(OrderApiData?.data?.total_amount).toFixed(2)}</div>
                       </div>
                     </div>
                   </div>
@@ -495,8 +497,8 @@ export function ViewTickets(props: ViewTicketsType) {
                 </Button>
               </Link>
             </DialogHeader>
-            <DialogDescription className="relative  px-8 py-6  bg-card h-full rounded-lg  overflow-y-scroll scroll-hide">
-              <div className="h-full text-white " id="divToPrint">
+            <DialogDescription className="relative  px-8 py-6  bg-card whiteText h-full rounded-lg  overflow-y-scroll scroll-hide">
+              <div className="h-full" id="divToPrint">
                 <div className="flex flex-col md:flex-row items-center justify-between">
                   <div className="flex items-center">
                     <NextImage
@@ -505,7 +507,7 @@ export function ViewTickets(props: ViewTicketsType) {
                       alt="Logo"
                     />
                   </div>
-                  <div className="text-white xs:text-center sm:text-left">
+                  <div className="xs:text-center sm:text-left">
                     <div className="font-bold text-xl mb-2">INVOICE</div>
                     <div className="text-sm">
                       Date: {displayDate(props?.selectedOrderEvent?.created_at)}
