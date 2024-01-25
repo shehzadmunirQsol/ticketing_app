8265f51597698f5ed6428ce30e395858ab5a7c0f
Tholhath <tholhath@lemonadverts.com>
2024-01-25 10:40:36 +0400

    payment

--- src/server/routers/order.ts
+++ src/server/routers/order.ts
@@ -808,9 +808,16 @@ export const orderRouter = router({
           //     .toLowerCase()
           //     .includes('success');
 
-           const declinedStatus = statusData?.result?.description.toLowerCase().includes('declined');
 
-          if (!declinedStatus) {
+          // const declinedStatus = statusData?.result?.description.toLowerCase().includes('declined');
+          // if (!declinedStatus) {
+          // }
+
+          
+          // const regexPattern = /^(000\.000\.|000\.100\.1|000\.[36]|000\.400\.[1][12]0)/;
+          // if (regexPattern.test(statusData?.result?.code)===true)
+
+          if (statusData?.result?.code==="000.000.000" || statusData?.result?.code==="000.100.110") {
             const payload = JSON.parse(statusData?.customParameters?.payload);
             const customerData = await prisma.customer.findFirst({
               where: {
