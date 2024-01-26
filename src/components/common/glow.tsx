933c78185481b6345fdd51190ed54124fc76264d
ahmarsm <ahmar@lemonadverts.com>
2024-01-26 14:35:53 +0400

    sas

--- src/components/common/glow.tsx
+++ src/components/common/glow.tsx
@@ -4,6 +4,8 @@ const Glow = ({ className }: any) => {
   return (
     <div
       className={className + ' circlebx'}
+      // bg-teal-400 bg-opacity-30 rounded-full blur-3xl
+      // circlebx
     ></div>
   );
 };
