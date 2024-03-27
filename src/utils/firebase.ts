a300dcc50336f3732eb59d99b8be60b059f9d2fd
unknown <shehzadmunir.qsols@gmail.com>
2024-03-27 12:19:15 +0500

    work on  json data

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,6 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getMessaging } from 'firebase/messaging';
 import admin from 'firebase-admin';
+const serviceAccount = require('~/utils/ticketing-25ba6-firebase.json');
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -20,10 +21,11 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      privateKey: process.env?.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      clientEmail: process.env?.FIREBASE_CLIENT_EMAIL,
-    }),
+    // credential: admin.credential.cert({
+    //   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    //   clientEmail: process.env?.FIREBASE_CLIENT_EMAIL,
+    //   privateKey: process.env?.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    // }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
